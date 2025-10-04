9fa4e00b56dd10ba8e0d291514cfff07cfbfd4e7
Damian Kęska <372403+keskad@users.noreply.github.com>
2025-10-04 19:07:26 +0200

    chore: Fix formating

--- pipelines_feedback_tekton.tf
+++ pipelines_feedback_tekton.tf
@@ -6,22 +6,22 @@ resource "github_repository" "pipelines_feedback_tekton" {
   name        = "pipelines-feedback-tekton"
   description = "Kubernetes controller - Tekton Pipelines integration with your favorite SCM - see commit & PR status right in Gitlab/Github/Bitbucket/etc."
   visibility  = "public"
-  topics      = [
-      "bitbucket",
-      "gitea",
-      "github",
-      "gitlab",
-      "gitops",
-      "go",
-      "jenkins-x",
-      "kubernetes",
-      "openshift",
-      "openshift-pipelines",
-      "pipelines-feedback",
-      "scm",
-      "tekton",
-      "tekton-pipelines",
-      "tektoncd"
+  topics = [
+    "bitbucket",
+    "gitea",
+    "github",
+    "gitlab",
+    "gitops",
+    "go",
+    "jenkins-x",
+    "kubernetes",
+    "openshift",
+    "openshift-pipelines",
+    "pipelines-feedback",
+    "scm",
+    "tekton",
+    "tekton-pipelines",
+    "tektoncd"
   ]
 
   allow_update_branch    = true
